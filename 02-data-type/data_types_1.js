4ff77354c1cdc3f37ffda8b90fd9b3b8fca831ab
devleandrodias <leandrodbdias@gmail.com>
2022-11-17 17:12:05 -0300

    feat: finished class about data types

---
+++ 02-data-type/data_types_1.js
@@ -0,0 +1,6 @@
+console.log(typeof 10);
+console.log(typeof "JavaScript");
+console.log(typeof true);
+console.log(typeof Symbol("iterator"));
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 02-data-type/data_types_3.js
@@ -0,0 +1,10 @@
+console.log(
+  typeof function sum(a, b) {
+    return a + b;
+  }
+);
+
+console.log(typeof { name: "Linus Torvalds" });
+console.log(typeof [1, 2, 3, 4, 5, 6]);
+console.log(typeof /[a-zA-Z]+/);
+console.log(typeof new Date());
